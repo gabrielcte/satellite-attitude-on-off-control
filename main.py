0ca8579494791743ca23f8c8ffbc850e7301aefa
Gabriel Alves Silva <gabriel_cte@outlook.com>
2026-05-27 20:04:33 -0300

    feat(integrator): Adição do integrador RK4 e comparação com Euler
    Signed-off-by: Gabriel Alves Silva <gabriel_cte@outlook.com>

--- main.py
+++ main.py
@@ -6,8 +6,31 @@ import control as ctrl
 import numpy as np
 import matplotlib.pyplot as plt
 
+
 def Euler(x, x_d, dt):
     return x + x_d*dt
+
+
+def RK4(x, x_d, dt):
+    k1 = x_d
+    k2 = x_d
+    k3 = x_d
+    k4 = x_d
+
+    return x + (dt/6)*(k1 + 2*k2 + 2*k3 + k4)
+
+
+def actuator_dynamics(f, u, FM, TC):
+    return (FM*u - f)/TC
+
+
+def satellite_dynamics(f, FD, I):
+    return (f - FD)/I
+
+
+def sensor_dynamics(thet, thet_s, thet_s_d, omega, zeta):
+    return (omega**2*(thet - thet_s) - 2*zeta*omega*thet_s_d)
+
 
 if __name__ == '__main__':
 
@@ -25,7 +48,6 @@ if __name__ == '__main__':
 
     # TRANSFER FUNCTIONS
     s = ctrl.TransferFunction.s
-
     G_act = FM/(TC*s + 1)
     G_sat = 1/(I*s)
     G_sensor = omega**2/(s**2 + 2*zeta*omega*s + omega**2)
@@ -33,9 +55,7 @@ if __name__ == '__main__':
 
     # SYSTEM FREQUENCY
     poles = ctrl.poles(G_open)
-
     wn = []
-
     for p in poles:
         wn.append(abs(p))
 
@@ -44,114 +64,175 @@ if __name__ == '__main__':
     f_nyquist = 2*f_max
     fs = 20*f_max
     dt = 1/fs
+
     print('\nPoles:')
     print(poles)
-
     print('\nMaximum Frequency (rad/s):', wn_max)
     print('\nMaximum Frequency (Hz):', f_max)
     print('\nNyquist Frequency (Hz):', f_nyquist)
     print('\nSampling Frequency (Hz):', fs)
     print('\nSimulation Step dt (s):', dt)
+
     # SIMULATION PARAMETERS
-    tf = 20                 # (s) Final simulation time
+    tf = 10                 # (s) Final simulation time
     N = int(tf/dt)          # Number of simulation steps
     delay_steps = int(TD/dt)
 
-    # PROBLEM VARIABLES
-    thet = 0                # (rad) Satellite attitude angle
-    thet_d = 0              # (rad/s) Satellite attitude rate
-    thet_dd = 0             # (rad/s²) Satellite attitude acceleration
-    thet_s = 0              # (rad) Angle sensor output
-    thet_s_d = 0            # (rad/s) Angle sensor output derivative
-    thet_s_dd = 0           # (rad/s²) Angle sensor output second derivative
-    thet_e = 0              # (rad) Control error
-    up = 0                  # Controller switch output
-    u = 0                   # Delayed switch output
-    f = 0                   # (N.m) Actuator torque
-    f_d = 0                 # (N.m/s) Actuator torque derivative
+    # EULER STATES
+    thet_eu = 0             # (rad) Satellite attitude angle
+    thet_d_eu = 0           # (rad/s) Satellite attitude rate
+    thet_s_eu = 0           # (rad) Angle sensor output
+    thet_s_d_eu = 0         # (rad/s) Angle sensor derivative
+    f_eu = 0                # (N.m) Actuator torque
+
+    # RK4 STATES
+    thet_rk = 0             # (rad) Satellite attitude angle
+    thet_d_rk = 0           # (rad/s) Satellite attitude rate
+    thet_s_rk = 0           # (rad) Angle sensor output
+    thet_s_d_rk = 0         # (rad/s) Angle sensor derivative
+    f_rk = 0                # (N.m) Actuator torque
+
+    # CONTROL VARIABLES
+    up_eu = 0               # Controller switch output
+    up_rk = 0               # Controller switch output
+
+    u_eu = 0                # Delayed switch output
+    u_rk = 0                # Delayed switch output
 
     # DELAY BUFFER
-    u_buffer = [0]*delay_steps
+    u_buffer_eu = [0]*delay_steps
+    u_buffer_rk = [0]*delay_steps
 
     # STORAGE VECTORS
     time_hist = []
-    thet_hist = []
-    thet_d_hist = []
-    thet_s_hist = []
-    thet_e_hist = []
-    u_hist = []
-    f_hist = []
+
+    thet_eu_hist = []
+    thet_rk_hist = []
+
+    thet_d_eu_hist = []
+    thet_d_rk_hist = []
+
+    thet_s_eu_hist = []
+    thet_s_rk_hist = []
+
+    u_eu_hist = []
+    u_rk_hist = []
+
+    f_eu_hist = []
+    f_rk_hist = []
+
 
     # SIMULATION LOOP
     for k in range(N):
+
         t = k*dt
 
-        # CONTROL ERROR
-        thet_e = theta_c - thet_s - KR*thet_d
+        # EULER CONTROL
+        thet_e_eu = theta_c - thet_s_eu - KR*thet_d_eu
+        if thet_e_eu > delta:
+            up_eu = 1
 
-        # ON-OFF CONTROL
-        if thet_e > delta:
-            up = 1
-
-        elif thet_e < -delta:
-            up = -1
+        elif thet_e_eu < -delta:
+            up_eu = -1
 
         else:
-            up = 0
+            up_eu = 0
 
-        # TIME DELAY
-        u_buffer.append(up)
-        u = u_buffer.pop(0)
+        u_buffer_eu.append(up_eu)
+        u_eu = u_buffer_eu.pop(0)
 
-        # DYNAMICS
-        f_d = (FM*u - f)/TC
-        thet_dd = (f - FD)/I
-        thet_s_dd = (omega**2*(thet - thet_s) - 2*zeta*omega*thet_s_d)
+        # EULER DYNAMICS
+        f_d_eu = actuator_dynamics(f_eu, u_eu, FM, TC)
+        thet_dd_eu = satellite_dynamics(f_eu, FD, I)
+        thet_s_dd_eu = sensor_dynamics(thet_eu,thet_s_eu,thet_s_d_eu, omega,zeta)
 
-        # INTEGRATION
-        f = Euler(f, f_d, dt)
-        thet_d = Euler(thet_d, thet_dd, dt)
-        thet = Euler(thet, thet_d, dt)
-        thet_s_d = Euler(thet_s_d, thet_s_dd, dt)
-        thet_s = Euler(thet_s, thet_s_d, dt)
+        # EULER INTEGRATION
+        f_eu = Euler(f_eu, f_d_eu, dt)
+        thet_d_eu = Euler(thet_d_eu, thet_dd_eu, dt)
+        thet_eu = Euler(thet_eu, thet_d_eu, dt)
+        thet_s_d_eu = Euler(thet_s_d_eu, thet_s_dd_eu, dt)
+        thet_s_eu = Euler(thet_s_eu, thet_s_d_eu, dt)
+
+        # RK4 CONTROL
+        thet_e_rk = theta_c - thet_s_rk - KR*thet_d_rk
+        if thet_e_rk > delta:
+            up_rk = 1
+
+        elif thet_e_rk < -delta:
+            up_rk = -1
+
+        else:
+            up_rk = 0
+
+        u_buffer_rk.append(up_rk)
+        u_rk = u_buffer_rk.pop(0)
+
+        # RK4 DYNAMICS
+        f_d_rk = actuator_dynamics(f_rk, u_rk, FM, TC)
+        thet_dd_rk = satellite_dynamics(f_rk, FD, I)
+        thet_s_dd_rk = sensor_dynamics(thet_rk, thet_s_rk, thet_s_d_rk, omega, zeta)
+
+        # RK4 INTEGRATION
+        f_rk = RK4(f_rk, f_d_rk, dt)
+        thet_d_rk = RK4(thet_d_rk, thet_dd_rk, dt)
+        thet_rk = RK4(thet_rk, thet_d_rk, dt)
+        thet_s_d_rk = RK4(thet_s_d_rk, thet_s_dd_rk, dt)
+        thet_s_rk = RK4(thet_s_rk, thet_s_d_rk, dt)
 
         # SAVE DATA
         time_hist.append(t)
-        thet_hist.append(thet)
-        thet_d_hist.append(thet_d)
-        thet_s_hist.append(thet_s)
-        thet_e_hist.append(thet_e)
-        u_hist.append(u)
-        f_hist.append(f)
+
+        thet_eu_hist.append(thet_eu)
+        thet_rk_hist.append(thet_rk)
+
+        thet_d_eu_hist.append(thet_d_eu)
+        thet_d_rk_hist.append(thet_d_rk)
+
+        thet_s_eu_hist.append(thet_s_eu)
+        thet_s_rk_hist.append(thet_s_rk)
+
+        u_eu_hist.append(u_eu)
+        u_rk_hist.append(u_rk)
+
+        f_eu_hist.append(f_eu)
+        f_rk_hist.append(f_rk)
 
     # PLOTS
     plt.figure(figsize=(12,10))
 
     # ANGLE
     plt.subplot(4,1,1)
-    plt.plot(time_hist, thet_hist, label='thet')
-    plt.plot(time_hist, thet_s_hist, label='thet_s')
-    plt.axhline(theta_c, linestyle='--', label='theta_c')
+    plt.plot(time_hist, thet_eu_hist,color='blue', label='thet Euler')
+    plt.plot(time_hist, thet_rk_hist,color='red', label='thet RK4')
+    plt.plot(time_hist, thet_s_eu_hist, '--',color='blue', label='thet_s Euler')
+    plt.plot(time_hist, thet_s_rk_hist, '--',color='red', label='thet_s RK4')
+    plt.axhline(theta_c, linestyle=':',color='black', label='theta_c')
     plt.ylabel('Angle (rad)')
     plt.grid()
     plt.legend()
 
     # RATE
     plt.subplot(4,1,2)
-    plt.plot(time_hist, thet_d_hist)
+    plt.plot(time_hist, thet_d_eu_hist,color='blue', label='Euler')
+    plt.plot(time_hist, thet_d_rk_hist,color='red', label='RK4')
     plt.ylabel('Rate (rad/s)')
     plt.grid()
+    plt.legend()
 
     # CONTROL
     plt.subplot(4,1,3)
-    plt.plot(time_hist, u_hist)
+    plt.plot(time_hist, u_eu_hist,color='blue', label='Euler')
+    plt.plot(time_hist, u_rk_hist,color='red', label='RK4')
     plt.ylabel('Control')
     plt.grid()
+    plt.legend()
 
     # TORQUE
     plt.subplot(4,1,4)
-    plt.plot(time_hist, f_hist)
+    plt.plot(time_hist, f_eu_hist,color='blue', label='Euler')
+    plt.plot(time_hist, f_rk_hist,color='red', label='RK4')
     plt.ylabel('Torque (N.m)')
     plt.xlabel('Time (s)')
     plt.grid()
+    plt.legend()
     plt.show()
